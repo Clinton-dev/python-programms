8cb2216f5bb12a7ea5583ea88054a7d51410a14e
Clinton wambugu <clintonwambugu@gmail.com>
2022-10-09 23:05:24 +0300

    Add doc string

--- decipherMultipleWords.py
+++ decipherMultipleWords.py
@@ -1,3 +1,10 @@
+"""
+Write a function that takes a string and returns and returns the deciphered string so that you can show the commander that the minions are talking about “Lance & Janice” instead of doing their jobs.
+did you see last night's episode?
+Yeah! I can't believe Lance lost his job at the colony!!
+"""
+
+
 def decipherOneWord(text):
     encrypt = 'abcdefghijklmnopqrstuvwxyz'
     word = [letter for letter in text]
